dae05d9e92ff2ed54210a76b3723a1f7d8ebf464
trapkey13 <trapkey13@gmail.com>
2025-12-23 18:28:45 +0200

    Update constants.js
    updated the expectations curves

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
-const GAME_1 = { id: "game1", diceResults: [6, 2, 5, 2, 4, 6] };
-const GAME_2 = { id: "game2", diceResults: [5, 4, 4, 1, 1, 2] };
-const GAME_3 = { id: "game3", diceResults: [2, 1, 5, 5, 6, 4] };
-const GAME_4 = { id: "game4", diceResults: [1, 3, 4, 4, 3, 5] };
-const GAME_5 = { id: "game5", diceResults: [3, 2, 6, 3, 3, 6] };
+const GAME_1 = { id: "game1", diceResults: [2, 2, 2, 6, 5, 3] };
+const GAME_2 = { id: "game2", diceResults: [4, 5, 5, 1, 2, 3] };
+const GAME_3 = { id: "game3", diceResults: [1, 4, 3, 5, 6, 6] };
+const GAME_4 = { id: "game4", diceResults: [5, 6, 2, 1, 2, 4] };
+const GAME_5 = { id: "game5", diceResults: [3, 4, 5, 1, 3, 6] };
 const PRACTICE_GAME = { id: "practice", diceResults: [6, 1, 3, 4, 1, 2] };
 const PREPARED_GAME_LIST = [GAME_1, GAME_2, GAME_3, GAME_4, GAME_5];

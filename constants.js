c63452607f67e9da14dd1ec48c6e2002f8874774
trapkey13 <trapkey13@gmail.com>
2025-12-23 20:24:26 +0200

    Update constants.js
    updated a mistake in the curve

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 const GAME_1 = { id: "game1", diceResults: [2, 2, 2, 6, 5, 3] };
 const GAME_2 = { id: "game2", diceResults: [4, 5, 5, 1, 2, 3] };
 const GAME_3 = { id: "game3", diceResults: [1, 4, 3, 5, 6, 6] };
-const GAME_4 = { id: "game4", diceResults: [5, 6, 2, 1, 2, 4] };
+const GAME_4 = { id: "game4", diceResults: [5, 6, 2, 4, 2, 4] };
 const GAME_5 = { id: "game5", diceResults: [3, 4, 5, 1, 3, 6] };
 const PRACTICE_GAME = { id: "practice", diceResults: [6, 1, 3, 4, 1, 2] };
 const PREPARED_GAME_LIST = [GAME_1, GAME_2, GAME_3, GAME_4, GAME_5];

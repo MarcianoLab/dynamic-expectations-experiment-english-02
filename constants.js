1897da3fe3ecd7e80e42eaaa318d70205dbd803d
trapkey13 <trapkey13@gmail.com>
2026-04-25 16:56:08 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,32 @@
+var HEBREW = false;
+
+var UI_TEXT = {
+    en: {
+        rollDice: "Roll Dice",
+        currentWinningChance: "Current winning<br>chance",
+        preStart: "Pre-start",
+        youWon: "You Won!",
+        youLose: "You Lose!",
+        satisfiedQuestion: "How satisfied are you at this moment?",
+        veryDissatisfied: "Very dissatisfied",
+        verySatisfied: "Very satisfied",
+        continue: "Continue",
+        sliderReminder: "Please move the slider according to the instructions",
+    },
+    he: {
+        rollDice: "להטיל קובייה",
+        currentWinningChance: "הסיכוי הנוכחי<br>לזכות",
+        preStart: "טרום משחק",
+        youWon: "!ניצחת",
+        youLose: "!הפסדת",
+        satisfiedQuestion: "?עד כמה את/ה מרוצה כרגע",
+        veryDissatisfied: "מאוד לא מרוצה",
+        verySatisfied: "מרוצה מאוד",
+        continue: "המשך/י",
+        sliderReminder: "גרור/י את המחוון למיקום המתאים",
+    },
+};
+
 var GAME_1 = { diceResults: [2, 2, 2, 6, 5, 3] };
 var GAME_2 = { diceResults: [4, 5, 5, 1, 2, 3] };
 var GAME_3 = { diceResults: [1, 4, 3, 5, 6, 6] };

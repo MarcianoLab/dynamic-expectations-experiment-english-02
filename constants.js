102ae86b63c9080de68f6c42c6649da42bc22d19
trapkey13 <trapkey13@gmail.com>
2026-04-25 17:14:49 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,11 @@
+var GAME_1 = { diceResults: [2, 2, 2, 6, 5, 3] };
+var GAME_2 = { diceResults: [4, 5, 5, 1, 2, 3] };
+var GAME_3 = { diceResults: [1, 4, 3, 5, 6, 6] };
+var GAME_4 = { diceResults: [5, 6, 2, 4, 2, 4] };
+var GAME_5 = { diceResults: [3, 4, 5, 1, 3, 6] };
+var PRACTICE_GAME = { diceResults: [6, 1, 3, 4, 1, 2] };
+var PREPARED_GAME_LIST = [GAME_1, GAME_2, GAME_3, GAME_4, GAME_5];
+
 var HEBREW = true;
 
 var UI_TEXT = {
@@ -15,22 +23,15 @@ var UI_TEXT = {
     },
     he: {
         rollDice: "להטיל קובייה",
-        currentWinningChance: "הסיכוי הנוכחי<br>לזכות",
+        currentWinningChance:"הסיכויים שלך לזכות",
         preStart: "טרום משחק",
-        youWon: "!ניצחת",
-        youLose: "!הפסדת",
-        satisfiedQuestion: "?עד כמה את/ה מרוצה כרגע",
+        youWon: "ניצחת!",
+        youLose: "הפסדת!",
+        satisfiedQuestion: "כמה את/ה מרוצה כרגע?",
         veryDissatisfied: "מאוד לא מרוצה",
-        verySatisfied: "מרוצה מאוד",
+        verySatisfied: "מאוד מרוצה",
         continue: "המשך/י",
         sliderReminder: "גרור/י את המחוון למיקום המתאים",
     },
 };
 
-var GAME_1 = { diceResults: [2, 2, 2, 6, 5, 3] };
-var GAME_2 = { diceResults: [4, 5, 5, 1, 2, 3] };
-var GAME_3 = { diceResults: [1, 4, 3, 5, 6, 6] };
-var GAME_4 = { diceResults: [5, 6, 2, 4, 2, 4] };
-var GAME_5 = { diceResults: [3, 4, 5, 1, 3, 6] };
-var PRACTICE_GAME = { diceResults: [6, 1, 3, 4, 1, 2] };
-var PREPARED_GAME_LIST = [GAME_1, GAME_2, GAME_3, GAME_4, GAME_5];

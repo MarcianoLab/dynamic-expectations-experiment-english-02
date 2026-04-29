045e2ea3fd3368f3955a0cc0d0c796bd5ebedeef
trapkey13 <trapkey13@gmail.com>
2026-04-29 11:19:31 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -6,7 +6,7 @@ var GAME_5 = { diceResults: [3, 4, 5, 1, 3, 6] };
 var PRACTICE_GAME = { diceResults: [6, 1, 3, 4, 1, 2] };
 var PREPARED_GAME_LIST = [GAME_1, GAME_2, GAME_3, GAME_4];
 
-var HEBREW = true;
+var HEBREW = false;
 
 var UI_TEXT = {
     en: {

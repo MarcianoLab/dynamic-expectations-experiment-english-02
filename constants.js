4cd9feb79041f36b68bb50682ce2948d3d2706fb
trapkey13 <trapkey13@gmail.com>
2026-05-03 16:45:25 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -2,9 +2,17 @@ var GAME_1 = { diceResults: [2, 2, 2, 6, 5, 3] };
 var GAME_2 = { diceResults: [4, 5, 5, 1, 2, 3] };
 var GAME_3 = { diceResults: [1, 4, 3, 5, 6, 6] };
 var GAME_4 = { diceResults: [5, 6, 2, 4, 2, 4] };
+
 var GAME_5 = { diceResults: [3, 4, 5, 1, 3, 6] };
+
+var GAME_6 = { diceResults: [2, 2, 2, 6, 5, 4] };
+var GAME_7 = { diceResults: [4, 5, 5, 1, 2, 6] };
+var GAME_8 = { diceResults: [1, 4, 3, 5, 6, 1] };
+var GAME_9 = { diceResults: [5, 6, 2, 4, 2, 1] };
+
 var PRACTICE_GAME = { diceResults: [6, 1, 3, 4, 1, 2] };
-var PREPARED_GAME_LIST = [GAME_1, GAME_2, GAME_3, GAME_4];
+
+var PREPARED_GAME_LIST = [GAME_1, GAME_2, GAME_3, GAME_4, GAME_6, GAME_7, GAME_8, GAME_9];
 
 var HEBREW = true;
 
